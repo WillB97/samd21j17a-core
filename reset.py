0112f34eb2efb653d24558828ebe66cb8c5badf6
WillB97 <will@rudi.org.uk>
2021-05-15 14:33:33 +0100

    Add a useful message to reset script

--- reset.py
+++ reset.py
@@ -4,6 +4,8 @@
 import sys
 import serial
 
+if len(sys.argv) < 2:
+    exit("Please supply the serial port to perform the reset on")
 com = serial.Serial(sys.argv[1], 1200)
 com.dtr=False
 com.close()
